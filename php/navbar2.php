898a1261cbd85a96a289302e9890ef9d4277a97b
Jeremy2203 <jeremy220603@gmail.com>
2024-11-14 16:38:06 -0500

    logo

--- php/navbar2.php
+++ php/navbar2.php
@@ -64,7 +64,7 @@
 
 <nav class="navbar navbar-expand-lg nav-2" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand mx-4" href="#">LOGO</a>
+        <a class="navbar-brand mx-4" href="#">AnimaL<i class="fa-solid fa-infinity"></i>p</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
